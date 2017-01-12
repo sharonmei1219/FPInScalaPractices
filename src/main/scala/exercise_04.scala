c27da6f5af5e32d73adc44b4e138c17df19c66b4
xuelan.mei <sharonmei1219@gmail.com>
2017-01-12 13:44:06 +0800

    deploy sbt succ

--- src/main/scala/exercise_04.scala
+++ src/main/scala/exercise_04.scala
@@ -20,24 +20,3 @@ sealed trait Option[+A]{
 }
 case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
-
-
-def mean(xs: Seq[Double]): Option[Double] =
-    if (xs.isEmpty) None
-    else Some(xs.sum / xs.length)
-
-//exercise 4.2
-def variance(xs: Seq[Double]): Option[Double] = 
-	mean(xs) flatMap(m => mean(xs map(x => math.pow(x - m, 2))))
-
-//exercise 4.3
-def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C) : Option[C] =
-	b flatMap(x => a map (f(_, x)))
-
-//exercise 4.4
-def sequence[A](a: List[Option[A]]): Option[List[A]] =
-	a.foldRight[Option[List[A]]](Some(Nil))((x, y) => map2(x, y)(_::_))
-
-//exercise 4.5
-def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] =
-	a.foldRight[Option[List[B]]](Some(Nil))((x, r) => map2(f(x), r)(_::_))
